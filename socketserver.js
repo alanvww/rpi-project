7b38306209677b3229f53d5d0472bbdaacfe406b
Alan Ren <10086000+alanvww@users.noreply.github.com>
2023-10-25 15:50:57 -0400

    update socket and frontend

--- socketserver.js
+++ socketserver.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const os = require('os');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,41 +11,27 @@ const io = socketIo(server);
 
 const PORT = 3000;
 
-const storage = multer.diskStorage({
-	destination: './uploads/',
-	filename: (req, file, cb) => {
-		cb(null, 'file-' + Date.now() + path.extname(file.originalname));
-	},
-});
-
-const upload = multer({ storage: storage });
-
 app.use(express.static('public'));
+app.use('/uploads', express.static('uploads'));
 
 io.on('connection', (socket) => {
 	console.log('A user connected');
 
-	socket.on('send-file', async (data) => {
-		const { file, targetIP } = data;
-		io.emit('receive-file', { file: file, ip: targetIP });
-	});
+	socket.on('send-file', (data) => {
+		const { filename, fileBuffer, targetIP } = data;
 
-	socket.on('disconnect', () => {
-		console.log('A user disconnected');
+		// Get the current server's IP address
+		const serverIP = getServerIP();
+
+		if (targetIP === serverIP || targetIP === 'localhost' || !targetIP) {
+			const writePath = path.join(__dirname, 'uploads', filename);
+			fs.writeFileSync(writePath, fileBuffer);
+
+			// Emit to all clients including sender
+			io.emit('file-received', { filename: filename });
+		}
 	});
 });
-
-app.post('/upload', upload.single('file'), (req, res) => {
-	if (req.file) {
-		const targetIP = req.body.ipAddress; // Extract the IP address from the form
-		io.emit('send-file', { file: req.file, targetIP: targetIP });
-		res.send('File uploaded and sent via socket.io');
-	} else {
-		res.redirect('/');
-	}
-});
-
-app.use('/uploads', express.static('uploads'));
 
 app.get('/uploaded-files', (req, res) => {
 	fs.readdir('./uploads', (err, files) => {
@@ -59,3 +45,15 @@ app.get('/uploaded-files', (req, res) => {
 server.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
 });
+
+function getServerIP() {
+	const networkInterfaces = os.networkInterfaces();
+	for (const name of Object.keys(networkInterfaces)) {
+		for (const net of networkInterfaces[name]) {
+			if (net.family === 'IPv4' && !net.internal) {
+				return net.address;
+			}
+		}
+	}
+	return 'localhost';
+}
